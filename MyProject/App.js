ff677e42cec5b8ce5b81dcfdaec7e1fbdc72319f
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-09-19 09:39:05 +0200

    fix

--- MyProject/App.js
+++ MyProject/App.js
@@ -4,13 +4,10 @@ import RegistrationScreen from "./Screens/RegistrationScreen";
 import image from "./Images/PhotoBG.png";
 
 export default function App() {
-  // const image = {
-  //   uri: "https://images.unsplash.com/photo-1585208798174-6cedd86e019a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
-  // };
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={{ image }}
+        source={image}
         resizeMode="cover"
         style={{ width: "100%", height: "100%" }}
       >
--- MyProject/Screens/LoginScreen.jsx
+++ MyProject/Screens/LoginScreen.jsx
@@ -11,8 +11,78 @@ function LoginScreen() {
       >
         <Text style={styles.text}>Sign in</Text>
       </Pressable>
-      <Text style={styles.textSignUp}>Don't have an account? Sign гз</Text>
+      <Text style={styles.textSignUp}>Don't have an account? Sign up</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 375,
+    height: 549,
+    top: 263,
+    borderColor: "red",
+    borderWidth: 1,
+    paddingTop: 100,
+    backgroundColor: "white",
+    borderTopStartRadius: 25,
+    borderTopEndRadius: 25,
+  },
+  photoBlok: {
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+    position: "absolute",
+    top: -60,
+    left: "34%",
+    borderRadius: 16,
+  },
+  buttonX: {
+    left: 105,
+    top: 80,
+  },
+  input: {
+    marginRight: 16,
+    marginLeft: 16,
+    marginBottom: 0,
+    marginTop: 16,
+    borderWidth: 1,
+    padding: 16,
+    width: 343,
+    height: 50,
+    borderColor: "#E8E8E8",
+    borderRadius: 12,
+    color: "black",
+    backgroundColor: "#F6F6F6",
+  },
+  icon: {
+    top: -35,
+    left: 320,
+  },
+  title: {
+    textAlign: "center",
+  },
+  button: {
+    marginTop: 43,
+    marginRight: 16,
+    marginLeft: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    elevation: 3,
+    width: 343,
+    height: 51,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+  },
+  text: {
+    color: "white",
+  },
+  textSignUp: {
+    textAlign: "center",
+    marginTop: 16,
+    marginBottom: 30,
+  },
+});
 export default LoginScreen;
