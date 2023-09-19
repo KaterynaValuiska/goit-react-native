2062c8f6dda315ebe4a3c655c2ab37f86718d848
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-09-19 16:01:48 +0200

    focus

--- MyProject/Screens/RegistrationScreen.jsx
+++ MyProject/Screens/RegistrationScreen.jsx
@@ -1,66 +1,102 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 function RegistrationScreen() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [focus, setFocus] = useState(true);
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
-  const signIn = () => {
+  const signUp = () => {
     console.debug("Welcome!");
+    console.debug(`${name}, ${email}, ${password}`);
   };
+  const onFocus = () => {
+    setFocus(true);
+  };
+  const onBlur = () => {
+    setFocus(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.photoBlok}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <View style={styles.photoBlok}>
+          <MaterialCommunityIcons
+            name={"plus-circle-outline"}
+            style={styles.buttonX}
+            size={25}
+            color="#FF6C00"
+            onPress={toggleShowPassword}
+          />
+        </View>
+        <Text style={styles.title}>Registration</Text>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <TextInput
+            style={!focus ? input : inputFocus}
+            placeholder="Login"
+            type="text"
+            value={name}
+            placeholderTextColor="#aaa"
+            autoComplete="text"
+            onChangeText={setName}
+            onFocus={onFocus}
+            onBlur={onBlur}
+          />
+          <TextInput
+            style={!focus ? input : inputFocus}
+            placeholder="email"
+            type="email"
+            placeholderTextColor="#aaa"
+            autoComplete="email"
+            onChangeText={setEmail}
+            onFocus={onFocus}
+            onBlur={onBlur}
+          />
+          <TextInput
+            style={!focus ? input : inputFocus}
+            placeholder="password"
+            type="password"
+            placeholderTextColor="#aaa"
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}
+            autoComplete="password"
+            onFocus={onFocus}
+            onBlur={onBlur}
+          />
+        </KeyboardAvoidingView>
         <MaterialCommunityIcons
-          name={"plus-circle-outline"}
-          style={styles.buttonX}
-          size={25}
-          color="#FF6C00"
+          name={showPassword ? "eye-off" : "eye"}
+          size={24}
+          color="#aaa"
+          style={styles.icon}
           onPress={toggleShowPassword}
         />
+        <Pressable style={styles.button} onPress={signUp}>
+          <Text style={styles.text}>Sign up</Text>
+        </Pressable>
+
+        <Text style={styles.textSignUp}>Do you have an account? Sign in</Text>
       </View>
-      <Text style={styles.title}>Registration</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Login"
-        type="text"
-        placeholderTextColor="#aaa"
-        autoComplete="text"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="email"
-        type="email"
-        placeholderTextColor="#aaa"
-        autoComplete="email"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="password"
-        type="password"
-        placeholderTextColor="#aaa"
-        secureTextEntry={!showPassword}
-        value={password}
-        onChangeText={setPassword}
-        autoComplete="password"
-      />
-      <MaterialCommunityIcons
-        name={showPassword ? "eye-off" : "eye"}
-        size={24}
-        color="#aaa"
-        style={styles.icon}
-        onPress={toggleShowPassword}
-      />
-      <Pressable style={styles.button} onPress={signIn}>
-        <Text style={styles.text}>Sign up</Text>
-      </Pressable>
-      <Text style={styles.textSignUp}>Do you have an account? Sign in</Text>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     width: 375,
@@ -83,20 +119,6 @@ const styles = StyleSheet.create({
   buttonX: {
     left: 105,
     top: 80,
-  },
-  input: {
-    marginRight: 16,
-    marginLeft: 16,
-    marginBottom: 0,
-    marginTop: 16,
-    borderWidth: 1,
-    padding: 16,
-    width: 343,
-    height: 50,
-    borderColor: "#E8E8E8",
-    borderRadius: 12,
-    color: "black",
-    backgroundColor: "#F6F6F6",
   },
   icon: {
     top: -35,
@@ -130,5 +152,35 @@ const styles = StyleSheet.create({
     marginBottom: 30,
   },
 });
+
+const input = {
+  marginRight: 16,
+  marginLeft: 16,
+  marginBottom: 0,
+  marginTop: 16,
+  borderWidth: 1,
+  padding: 16,
+  width: 343,
+  height: 50,
+  borderColor: "#E8E8E8",
+  borderRadius: 12,
+  color: "black",
+  backgroundColor: "#F6F6F6",
+};
+
+const inputFocus = {
+  marginRight: 16,
+  marginLeft: 16,
+  marginBottom: 0,
+  marginTop: 16,
+  borderWidth: 1,
+  padding: 16,
+  width: 343,
+  height: 50,
+  borderColor: "#FF6C00",
+  borderRadius: 12,
+  color: "black",
+  backgroundColor: "#ffffff",
+};
 
 export default RegistrationScreen;
