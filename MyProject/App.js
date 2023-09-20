73f2804fe7e0fb8623d3895834d008070f88ac90
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-09-20 14:26:17 +0200

    nav

--- MyProject/App.js
+++ MyProject/App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, ImageBackground } from "react-native";
 import RegistrationScreen from "./Screens/RegistrationScreen";
@@ -5,20 +9,28 @@ import image from "./Images/PhotoBG.png";
 import LoginScreen from "./Screens/LoginScreen";
 import PostsScreen from "./Screens/PostsScreen";
 
+const MainStack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={image}
-        resizeMode="cover"
-        style={{ width: "100%", height: "100%" }}
-      >
-        <LoginScreen />
-        {/* <RegistrationScreen /> */}
-        {/* <PostsScreen /> */}
-      </ImageBackground>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainStack.Navigator>
+        <View style={styles.container}>
+          <ImageBackground
+            source={image}
+            resizeMode="cover"
+            style={{ width: "100%", height: "100%" }}
+          >
+            <MainStack.Screen
+              name="Registration"
+              component={RegistrationScreen}
+            />
+            <MainStack.Screen name="Login" component={LoginScreen} />
+            <MainStack.Screen name="Home" component={PostsScreen} />
+          </ImageBackground>
+          <StatusBar style="auto" />
+        </View>
+      </MainStack.Navigator>
+    </NavigationContainer>
   );
 }
 
